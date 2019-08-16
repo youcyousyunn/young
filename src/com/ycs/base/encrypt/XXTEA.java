947aa367c1dca4d72ccd8d33523cff66376b1da2
youcyousyunn <731781984@qq.com>
2019-08-16 15:22:27 +0800

    更改细节

--- src/com/ycs/base/encrypt/XXTEA.java
+++ src/com/ycs/base/encrypt/XXTEA.java
@@ -8,56 +8,45 @@ import org.springframework.util.Base64Utils;
 import com.ycs.base.property.SystemPropertyConfigure;
 
 public class XXTEA {
-
-	private static String base64Encode(byte[] data) {
-		return Base64Utils.encodeToString(data);
-	}
-
-	private static byte[] base64Decode(String data) throws IOException {
-		return Base64Utils.decode(data.getBytes());
-	}
-
-	private static byte[] base64Decode(byte[] data) throws IOException {
-		return Base64Utils.decode(data);
-	}
-
-	public static byte[] encrypt(byte[] data, byte[] key) {
-		return data.length == 0 ? data : toByteArray(encrypt(toIntArray(data, true), toIntArray(key, false)), false);
-	}
-
-	public static String encryptWithBase64(byte[] data, byte[] key) {
-		return base64Encode(encrypt(data, key));
-	}
-
+	
 	/**
 	 * Base64加密
 	 * @param
 	 * @return String
 	 * @throws 
 	 */
-	public static String encryptWithBase64(String data, byte[] key, String encoding)
-			throws UnsupportedEncodingException {
+	public static String encryptWithBase64(String data, byte[] key, String encoding) throws UnsupportedEncodingException {
 		return base64Encode(encrypt(data.getBytes(), key));
 	}
 
-	public static byte[] decrypt(byte[] data, byte[] key) {
-		return data.length == 0 ? data : toByteArray(decrypt(toIntArray(data, false), toIntArray(key, false)), true);
+	private static String base64Encode(byte[] data) {
+		return Base64Utils.encodeToString(data);
 	}
-
-	/**
-	 * Base64解密
-	 * @param
-	 * @return byte[]
-	 * @throws 
-	 */
-	public static byte[] decryptWithBase64(String data, byte[] key, String encoding) throws IOException {
-		return null != data && data.length() != 0 ? decrypt(base64Decode(data), key) : new byte[0];
+	
+	public static byte[] encrypt(byte[] data, byte[] key) {
+		return data.length == 0 ? data : toByteArray(encrypt(toIntArray(data, true), toIntArray(key, false)), false);
 	}
+	
+	private static byte[] toByteArray(int[] data, boolean includeLength) {
+		int n = data.length << 2;
+		if (includeLength) {
+			int result = data[data.length - 1];
+			if (result > n) {
+				return null;
+			}
 
-	public static byte[] decryptWithBase64(byte[] data, byte[] key) throws IOException {
-		return data.length == 0 ? data : decrypt(base64Decode(data), key);
+			n = result;
+		}
+
+		byte[] arg4 = new byte[n];
+
+		for (int i = 0; i < n; ++i) {
+			arg4[i] = (byte) (data[i >>> 2] >>> ((i & 3) << 3) & 255);
+		}
+
+		return arg4;
 	}
-
+	
 	public static int[] encrypt(int[] v, int[] k) {
 		int n = v.length - 1;
 		if (n < 1) {
@@ -92,7 +81,45 @@ public class XXTEA {
 			return v;
 		}
 	}
+	
+	private static int[] toIntArray(byte[] data, boolean includeLength) {
+		int n = (data.length & 3) == 0 ? data.length >>> 2 : (data.length >>> 2) + 1;
+		int[] result;
+		if (includeLength) {
+			result = new int[n + 1];
+			result[n] = data.length;
+		} else {
+			result = new int[n];
+		}
 
+		n = data.length;
+
+		for (int i = 0; i < n; ++i) {
+			result[i >>> 2] |= (255 & data[i]) << ((i & 3) << 3);
+		}
+
+		return result;
+	}
+	
+	
+	/**
+	 * Base64解密
+	 * @param
+	 * @return byte[]
+	 * @throws 
+	 */
+	public static byte[] decryptWithBase64(String data, byte[] key, String encoding) throws IOException {
+		return null != data && data.length() != 0 ? decrypt(base64Decode(data), key) : new byte[0];
+	}
+	
+	public static byte[] decrypt(byte[] data, byte[] key) {
+		return data.length == 0 ? data : toByteArray(decrypt(toIntArray(data, false), toIntArray(key, false)), true);
+	}
+	
+	private static byte[] base64Decode(String data) throws IOException {
+		return Base64Utils.decode(data.getBytes());
+	}
+	
 	public static int[] decrypt(int[] v, int[] k) {
 		int n = v.length - 1;
 		if (n < 1) {
@@ -125,55 +152,30 @@ public class XXTEA {
 			return v;
 		}
 	}
-
-	private static int[] toIntArray(byte[] data, boolean includeLength) {
-		int n = (data.length & 3) == 0 ? data.length >>> 2 : (data.length >>> 2) + 1;
-		int[] result;
-		if (includeLength) {
-			result = new int[n + 1];
-			result[n] = data.length;
-		} else {
-			result = new int[n];
-		}
-
-		n = data.length;
-
-		for (int i = 0; i < n; ++i) {
-			result[i >>> 2] |= (255 & data[i]) << ((i & 3) << 3);
-		}
-
-		return result;
+	
+	
+	public static String encryptWithBase64(byte[] data, byte[] key) {
+		return base64Encode(encrypt(data, key));
 	}
 
-	private static byte[] toByteArray(int[] data, boolean includeLength) {
-		int n = data.length << 2;
-		if (includeLength) {
-			int result = data[data.length - 1];
-			if (result > n) {
-				return null;
-			}
-
-			n = result;
-		}
-
-		byte[] arg4 = new byte[n];
-
-		for (int i = 0; i < n; ++i) {
-			arg4[i] = (byte) (data[i >>> 2] >>> ((i & 3) << 3) & 255);
-		}
-
-		return arg4;
+	public static byte[] decryptWithBase64(byte[] data, byte[] key) throws IOException {
+		return data.length == 0 ? data : decrypt(base64Decode(data), key);
 	}
+
+	private static byte[] base64Decode(byte[] data) throws IOException {
+		return Base64Utils.decode(data);
+	}
+	
 
 	public static void main(String[] args) throws IOException {
-		String pw = "root";
-		String value = "XXTEA:adPEIxU5f6c=".substring("XXTEA:".length());
-		String enPw = encryptWithBase64(pw, SystemPropertyConfigure.XXTEA_KEY.getBytes(), null);
-		String depw = new String(decryptWithBase64(enPw, SystemPropertyConfigure.XXTEA_KEY.getBytes(), null));
-		System.out.println(value);
-		System.out.println(pw);
-		System.out.println(enPw);
-		System.out.println(depw);
+		String pwd = "root";
+		System.out.println("明文密码："+pwd);
+		String encriptPwd = encryptWithBase64(pwd, SystemPropertyConfigure.XXTEA_KEY.getBytes(), null);
+		System.out.println("加密后密码："+encriptPwd);
+		String decryptPwd = new String(decryptWithBase64(encriptPwd, SystemPropertyConfigure.XXTEA_KEY.getBytes(), null));
+		System.out.println("解密后密码："+decryptPwd);
 	}
+	
+	
 
 }
